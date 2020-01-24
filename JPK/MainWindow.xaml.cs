749ac881b7753197e6068abedbed20c68bffa153
Paweł Bucior <pawel@bucior.eu>
2020-01-24 18:25:40 +0100

    Adding files

--- JPK/MainWindow.xaml.cs
+++ JPK/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace JPK
 {
@@ -20,9 +15,79 @@ namespace JPK
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Plik> observableCollectionListaPlikow = new ObservableCollection<Plik>();
+
         public MainWindow()
         {
             InitializeComponent();
+            listBoxPliki.ItemsSource = observableCollectionListaPlikow;
+        }
+
+        private void buttonDodajPlik_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki XML (*.XML)| *.XML";
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Wybierz pliki do wczytania";
+            openFileDialog.FileName = "";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                int iloscPominietych = 0;
+                StringBuilder pominietePliki = new StringBuilder();
+
+                foreach (String file in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        if (observableCollectionListaPlikow.Count > 0)
+                        {
+                            bool czyIstnieje = false;
+                            foreach (var item in observableCollectionListaPlikow)
+                            {
+                                if (String.Equals(file, item.Nazwa))
+                                {
+                                    czyIstnieje = true;
+                                    iloscPominietych++;
+                                    pominietePliki.AppendLine(file);
+                                }
+                            }
+                            if (!czyIstnieje)
+                            {
+                                dodajElementDoListy(file, observableCollectionListaPlikow);
+                                listBoxPliki.UnselectAll();
+                            }
+                        }
+                        else
+                        {
+                            dodajElementDoListy(file, observableCollectionListaPlikow);
+                            listBoxPliki.UnselectAll();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+                if (iloscPominietych > 0)
+                {
+                    String naglowek = "JPK";
+                    String message = "Następujące pliki były dodane wcześniej i zostały pominięte:"
+                        + Environment.NewLine
+                        + pominietePliki.ToString();
+                    MessageBox.Show(message, naglowek, MessageBoxButton.OK);
+                }
+            }
+        }
+
+        private void dodajElementDoListy(String element, ObservableCollection<Plik> listaPlikow)
+        {
+            Plik elementListy = new Plik();
+            elementListy.Status = true;
+            elementListy.CzyPolaczony = false;
+            elementListy.IdPliku = "";
+            elementListy.Nazwa = element;
+            listaPlikow.Add(elementListy);
         }
     }
 }
